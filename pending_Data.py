364c423b05689bef98746d3c1e2d989977e0cf63
GMCHNKL <73467282+GMCHNKL@users.noreply.github.com>
2021-01-04 16:23:02 +0530

    Update pending_Data.py

--- pending_Data.py
+++ pending_Data.py
@@ -2,11 +2,6 @@ import pandas as pd
 from os import path
 from os import listdir
 from os.path import isfile, join
-    
-import pandas as pd
-from os import path
-from os import listdir
-from os.path import isfile, join
 
 BaseDir = '/content/drive/MyDrive/Find_Pending_Icmr-master/'
 
